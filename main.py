9b0ceb1cfef4b389e2b661755c5fd7b51fa96e1c
Juraj Beňo <117287090+JurajBeno@users.noreply.github.com>
2023-05-02 17:40:18 +0200

    Update main.py
    deleted token literal

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 
 from cogs.Csgo_market import CsgoMarketCog
 
-TOKEN = "MTA2NTc0NTIzOTY2NDUwMDg0Ng.G3ljtb.f3beHyRlVeNsuWzfdqN7rkKAnfltK4li_WP5Vw"
+TOKEN = ""
 DESC = "cutie, dont hate on me"
 
 class MarketBot(commands.Bot):
